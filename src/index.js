42f0ec4c9fc7409e6614c6c266e7ec2f0b68270a
Toma <novotny.tom96@gmail.com>
2025-06-21 13:53:47 +0200

    Action setup

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const actions = require('@actions/core');
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+
+async function main() {
+    try {
+        const filePath = actions.getInput('file');
+        const fileEncoding = actions.getInput('encoding');
+
+        const fileContentBuffer = await readFile(filePath, fileEncoding);
+        const fileContent = fileContentBuffer.toString(fileEncoding);
+
+        const content = postProcessFileContent(fileContent);
+        actions.info(`Processed file content:\n${content}`);
+        actions.setOutput('content', content);
+    } catch (e) {
+        actions.setFailed(`File reading failed: ${e}`);
+    }
+}
+
+function postProcessFileContent(content) {
+    const lineAdjusted = applyLineLimit(content);
+    return applyCharacterLimit(lineAdjusted);
+}
+
+function applyLineLimit(content) {
+    const lineLimit = parseInt(actions.getInput('max-lines'), 10);
+    if (lineLimit <= 0) {
+        return content;
+    }
+    actions.info(`Action output is limited to ${lineLimit} lines, trimming file content`);
+    const lines = content.split(/\r?\n/);
+    if (lines.length <= lineLimit) {
+        return content;
+    }
+    return lines.slice(0, lineLimit).join('\n');
+}
+
+function applyCharacterLimit(content) {
+    const characterLimit = parseInt(actions.getInput('max-chars'), 10);
+    if (characterLimit <= 0 || content.length <= characterLimit) {
+        return content;
+    }
+
+    return content.slice(0, characterLimit);
+}
+
+main();
